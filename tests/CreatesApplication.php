c80f1c840c0949c4dd959d631a95f1ad32783db8
Augusto Rende <augustorendegiacomelli@gmail.com>
2018-08-21 09:42:40 -0300

    Migtation to Laravel 5.6

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -14,9 +14,13 @@ trait CreatesApplication
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
-
+ 
         $app->make(Kernel::class)->bootstrap();
-
+     
+        Hash::driver('bcrypt')->setRounds(4);
+     
         return $app;
     }
 }
+
+
